64d15557bd20237105121a8e7ad3a600ec3d33e7
0tve <grisha.polsky@gmail.com>
2024-11-15 00:25:05 +0300

    created maps endpoint

--- scripts/get_maps.py
+++ scripts/get_maps.py
@@ -7,10 +7,10 @@ from sklearn.linear_model import LogisticRegression
 from src.model import get_features_coefs_by_class
 
 if __name__ == '__main__':
-    output_path = 'output/'
+    data_path = 'data/'
     print('Введите имя csv-файла: ', end='')
     csv_name = input()
-    df = pd.read_csv(f'{output_path}{csv_name}.csv')
+    df = pd.read_csv(f'{data_path}{csv_name}.csv')
     value_counts = df['professional_role_name'].value_counts()
     df = df[df['professional_role_name'].isin(value_counts[value_counts >= 1000].index)]
     X = df['key_skill_name']
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     X, y = undersampler.fit_resample(X, y)
     model = LogisticRegression(max_iter=1000, random_state=42)
     model.fit(X, y)
-    key_skills_coefs_by_professional_role = get_features_coefs_by_class(model, X.columns, 10)
+    maps = get_features_coefs_by_class(model, X.columns, 10)
     
-    with open(f'{output_path}key_skills_coefs_by_professional_role.json', 'w+', encoding='utf-8') as f:
-        json.dump(key_skills_coefs_by_professional_role, f, ensure_ascii=False, indent=4)
+    with open(f'{data_path}maps.json', 'w+', encoding='utf-8') as f:
+        json.dump(maps, f, ensure_ascii=False, indent=4)
     
